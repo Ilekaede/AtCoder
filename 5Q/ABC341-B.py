bafe15ae02a8c8af5f596f903bdfb138eb232e6f
Ilekaede <asakuramotiyo@gmail.com>
2024-04-24 23:44:15 +0900

    add:ABC341B

---
+++ 5Q/ABC341-B.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = list(map(int, input().split()))
+ST = [tuple(map(int, input().split())) for _ in range(N - 1)]
+
+for i in range(N - 1):
+    S, T = ST[i]
+    A[i + 1] = (A[i] // S) * T
+
+print(A[N - 1])
+
